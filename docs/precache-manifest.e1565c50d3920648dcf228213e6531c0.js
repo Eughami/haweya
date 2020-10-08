8fcde96335b88bb459ad0d1ea47098e8fed3e379
mbl <imamosi50@gmail.com>
2020-10-08 17:16:05 +0300

    build file

---
+++ docs/precache-manifest.e1565c50d3920648dcf228213e6531c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f8440f7ede2a5310fa461e254486e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c94634f66fe4b554a3c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e89a56ce6555e36c956",
+    "url": "/static/js/2.414a7b0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.414a7b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c94634f66fe4b554a3c",
+    "url": "/static/js/main.808b2378.chunk.js"
+  },
+  {
+    "revision": "92ce678b6561e85f4a28",
+    "url": "/static/js/runtime-main.3683e5b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
