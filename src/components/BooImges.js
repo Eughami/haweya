903f618762f289d49b352529776f0afaa0ccbc1a
Eughami <imamosi50@gmail.com>
2020-10-10 01:20:27 +0300

    added image page and video pages

--- src/components/BooImges.js
+++ src/components/BooImges.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
 
+import image1 from '../images/cuet2.jpg'
+import image2 from '../images/cute1.jpg'
+import image3 from '../images/cute2.jpg'
+import image4 from '../images/favorite.jpg'
+import image5 from '../images/favorite1.jpg'
+import image6 from '../images/kawai.jpg'
+import image7 from '../images/loginBackground1.jpg'
+import image8 from '../images/mainBackground1.jpg'
+import image9 from '../images/nice.jpg'
+import image10 from '../images/owwwww.jpg'
 class BooImges extends Component {
   render() {
     return (
       <div>
-        <h1>BooImges</h1>
+        <AliceCarousel autoPlay autoPlayInterval="3000" disableButtonsControls={true}>
+          <img src={image1} className="sliderimg"/>
+          <img src={image2} className="sliderimg"/>
+          <img src={image3} className="sliderimg"/>
+          <img src={image4} className="sliderimg"/>
+          <img src={image5} className="sliderimg"/>
+          <img src={image6} className="sliderimg"/>
+          <img src={image7} className="sliderimg"/>
+          <img src={image8} className="sliderimg"/>
+          <img src={image9} className="sliderimg"/>
+          <img src={image10} className="sliderimg"/>
+      </AliceCarousel>
       </div>
     );
   }
--- src/components/BooVideo.js
+++ src/components/BooVideo.js
@@ -3,8 +3,12 @@ import React, { Component } from 'react';
 class BooVideo extends Component {
   render() {
     return (
-      <div>
-        <h1>Video</h1>
+      <div className='video__page__container'>
+        <span className='video__page__message'>
+          There was suppose to be a video here But i coudln't complete it in time
+          Sorry Babe and wish you a wonderful Day<br/>
+          Your ever loving Mamou ❤️
+        </span>
       </div>
     );
   }
