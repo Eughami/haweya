0c107620e6eff3188829c61eb3a19dac8f4e3a73
Eughami <imamosi50@gmail.com>
2020-10-10 00:40:46 +0300

    added the letter/build

---
+++ docs/precache-manifest.33da622de1e0f11a5189c5e751f62bd1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299fff655f029051d5f22557ca731c93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1d60543182f5c92b26a",
+    "url": "/static/css/2.8f085498.chunk.css"
+  },
+  {
+    "revision": "318e6e449b8913183b3a",
+    "url": "/static/css/main.96461436.chunk.css"
+  },
+  {
+    "revision": "d1d60543182f5c92b26a",
+    "url": "/static/js/2.006af6bb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.006af6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318e6e449b8913183b3a",
+    "url": "/static/js/main.bc0648a9.chunk.js"
+  },
+  {
+    "revision": "92ce678b6561e85f4a28",
+    "url": "/static/js/runtime-main.3683e5b1.js"
+  },
+  {
+    "revision": "efe3f39cf033d95298706ab8f2b5380e",
+    "url": "/static/media/letter.efe3f39c.jpg"
+  },
+  {
+    "revision": "502746de1cf1723c9eb7d5c2346c6ce4",
+    "url": "/static/media/loginBackground.502746de.jpg"
+  },
+  {
+    "revision": "f4d1d71f24321c9b45d92930f3ac7515",
+    "url": "/static/media/mainBackground.f4d1d71f.jpeg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1565c50d3920648dcf228213e6531c0.js"
+  "/precache-manifest.33da622de1e0f11a5189c5e751f62bd1.js"
 );
 
 self.addEventListener('message', (event) => {
